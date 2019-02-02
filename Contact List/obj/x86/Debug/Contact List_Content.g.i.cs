e1828425e66c3524f80b17dd6c97793b00c79e5e
Mohamed Yahia <mohamed.yahia1982@gmail.com>
2019-02-02 21:55:55 +0200

    Add contact list

--- Contact List/obj/x86/Debug/Contact List_Content.g.i.cs
+++ Contact List/obj/x86/Debug/Contact List_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/phonebook.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("user_group.ico")]
 
 
